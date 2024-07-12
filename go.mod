eebf86304e8c844f19f53328a7903340defa4cc4
stevencol <steven2088@hotmail.es>
2024-07-12 16:32:05 -0500

    feat: creacion del api mid  base
    Se crea api mid base para para el módulo de revisión cumplidos proveedores
    
    udistrital/Cumplidos_y_certificaciones_documentacion#81

---
+++ go.mod
@@ -0,0 +1,7 @@
+
+module revision_cumplidos_proveedores_mid
+
+go 1.17
+
+require github.com/astaxie/beego v1.12.1
+require github.com/smartystreets/goconvey v1.6.4
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "revision_cumplidos_proveedores_mid/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
