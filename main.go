9f39101e4c478cfcfa9edd7c34db4e8f0ad4b0b1
stevencol <steven2088@hotmail.es>
2024-07-12 23:07:23 -0500

    feat: Importacion de librerias Se importan librerias   apistatus  "github.com/udistrital/utils_oas/apiStatusLib" "github.com/udistrital/utils_oas/customerrorv2" auditoria "github.com/udistrital/auditoria"
    udistrital/Cumplidos_y_certificaciones_documentacion#81

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	_ "revision_cumplidos_proveedores_mid/routers"
-
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/astaxie/beego"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerrorv2"
+	auditoria "github.com/udistrital/auditoria"
 )
 
 func main() {
+	AllowedOrigins := []string{"*.udistrital.edu.co"}
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: AllowedOrigins,
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	beego.ErrorController(&customerrorv2.CustomErrorController{})
 	apistatus.Init()
+	auditoria.InitMiddleware()
 	beego.Run()
 }
