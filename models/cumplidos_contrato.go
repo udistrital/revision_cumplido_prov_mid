9650ebba44dda4b10aaf86ff94375cd5a67be3fa
faidercamilo26 <olayafaider@gmail.com>
2024-09-16 16:43:41 -0500

    feature: se realiza el cambio de modelo para que en las solicitudes se retorne el codigo de abreviacion

--- models/cumplidos_contrato.go
+++ models/cumplidos_contrato.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type CumplidosContrato struct {
-	ConsecutivoCumplido int                `json:"ConsecutivoCumplido"`
-	NumeroContrato      string             `json:"NumeroContrato"`
-	FechaCreacion       time.Time          `json:"FechaCreacion"`
-	Periodo             string             `json:"Periodo"`
-	EstadoCumplido      string             `json:"EstadoCumplido"`
-	CumplidoProveedorId *CumplidoProveedor `json:"CumplidoProveedorId"`
+	ConsecutivoCumplido             int                `json:"ConsecutivoCumplido"`
+	NumeroContrato                  string             `json:"NumeroContrato"`
+	FechaCreacion                   time.Time          `json:"FechaCreacion"`
+	Periodo                         string             `json:"Periodo"`
+	EstadoCumplido                  string             `json:"EstadoCumplido"`
+	CodigoAbreviacionEstadoCumplido string             `json:"CodigoAbreviacionEstadoCumplido"`
+	CumplidoProveedorId             *CumplidoProveedor `json:"CumplidoProveedorId"`
 }
